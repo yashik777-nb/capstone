04ba44b8074ae65c477913761cfbf9f58bd41b38
Yash <yaswanth.iddibani@gmail.com>
2021-02-24 22:00:20 +0000

    Initial Project Setup

---
+++ src/container/App.js
@@ -0,0 +1,11 @@
+import AppCSS from "./App.module.css";
+
+function App() {
+  return (
+    <div className="container">
+      <button className="btn btn-primary">Check Bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
