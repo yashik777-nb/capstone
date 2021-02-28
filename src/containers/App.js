bb7694a05ae2ba03f6bba8170403ed6cc9d6bbe4
Yash <yaswanth.iddibani@gmail.com>
2021-02-28 12:09:01 +0000

    Lazy Loading

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React from "react";
-import "./App.css";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NewIssue from "../containers/NewIssue/NewIssue";
-import NotFound from "../containers/NotFound/NotFound";
-import AllIssues from "../containers/IssuesList/AllIssues";
-import About from "../components/About/About";
-import IssueDetail from "../containers/IssuesList/IssueDetail/IssueDetail";
-import SignInForm from "./Users/SignIn/SignInForm";
-import RegistrationForm from "./Users/Registration/RegistrationForm";
-import NavigationBar from "./NavigationBar/NavigationBar";
 import { connect } from "react-redux";
+import "./App.css";
 import * as actionCreators from "../store/actions/actionCreators/IssuesActionCreator";
+// import AllIssues from "../containers/IssuesList/AllIssues";
+// import NotFound from "../containers/NotFound/NotFound";
+// import About from "../components/About/About";
+// import NewIssue from "../containers/NewIssue/NewIssue";
+// import IssueDetail from "../containers/IssuesList/IssueDetail/IssueDetail";
+// import SignInForm from "./Users/SignIn/SignInForm";
+// import RegistrationForm from "./Users/Registration/RegistrationForm";
+// import NavigationBar from "./NavigationBar/NavigationBar";
+const AllIssues = lazy(() => import("../containers/IssuesList/AllIssues"));
+const NotFound = lazy(() => import("../containers/NotFound/NotFound"));
+const About = lazy(() => import("../components/About/About"));
+const IssueDetail = lazy(() =>
+  import("../containers/IssuesList/IssueDetail/IssueDetail")
+);
+const SignInForm = lazy(() => import("./Users/SignIn/SignInForm"));
+const RegistrationForm = lazy(() =>
+  import("./Users/Registration/RegistrationForm")
+);
+const NavigationBar = lazy(() => import("./NavigationBar/NavigationBar"));
+const NewIssue = lazy(() => import("../containers/NewIssue/NewIssue"));
 
 class App extends React.Component {
   componentDidMount() {
@@ -19,20 +32,22 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <NavigationBar />
-          <Switch>
-            <Route path="/addIssue" component={NewIssue} />
-            <Route path="/about" component={About} />
-            <Route path="/signin" component={SignInForm} />
-            <Route path="/register" component={RegistrationForm} />
-            <Route path="/issues/:issueDescription" component={IssueDetail} />
-            <Route path="/" component={AllIssues} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <BrowserRouter>
+          <div className="App">
+            <NavigationBar />
+            <Switch>
+              <Route path="/addIssue" component={NewIssue} />
+              <Route path="/about" component={About} />
+              <Route path="/signin" component={SignInForm} />
+              <Route path="/register" component={RegistrationForm} />
+              <Route path="/issues/:issueDescription" component={IssueDetail} />
+              <Route path="/" component={AllIssues} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Suspense>
     );
   }
 }
